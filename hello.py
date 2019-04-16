b8bc8f950f8507c52d019c13935e7f68b4132aed
kotk100 <kern.ziga@gmail.com>
2019-04-16 20:05:54 +0200

    PID for distance and rotation

--- hello.py
+++ hello.py
@@ -39,6 +39,95 @@ def set_font(name):
     '''
     os.system('setfont ' + name)
 
+def get_angle(gyro, readings_to_average):
+    angle = 0.0
+    for _ in range(readings_to_average):
+        angle += gyro.value()
+    return angle/readings_to_average
+
+def rotate_to_angle(angle, mL, mR, gyro):
+    debug_print("Rotate to ", angle, "degrees")
+    mL.position = mR.position = 0
+    e_old = integral = 0
+    t_beg = t_old = time.time()
+    e = angle - get_angle(gyro, 3)
+    k_p = 2
+    k_i = 3
+    k_d = 0
+    errors = 0xff
+
+    while errors: 
+        t = time.time()
+         # increase to 100 over 1.5 seconds
+        max_power = max(min((t - t_beg)* 100/1.5, 100), -100)
+
+        dT = t - t_old
+        e = angle - get_angle(gyro, 3)
+        errors = ((errors << 1) & 0xff) | (e != 0)
+        dE = e - e_old
+        u = k_p * e + max(min(k_i * integral, 10), -10) + k_d * dE / dT
+
+        u = max(min(u, max_power), -max_power)
+        debug_print(t, e, dE, integral, u, sep=', ')
+        mR.run_direct(duty_cycle_sp=-u)
+        mL.run_direct(duty_cycle_sp=u)
+
+        integral += e * dT
+        integral = max(min(integral, 10), -10)
+        e_old = e
+        t_old = t
+
+    mR.run_direct(duty_cycle_sp=0)
+    mL.run_direct(duty_cycle_sp=0)
+
+def drive_for_centimeters(distance, mL, mR, gyro, angle):
+    distance_count = (distance*mR.count_per_rot)/17.6
+    distance_count = int(round(distance_count))
+    debug_print("Move for ", distance_count, "degrees")
+    mL.position = mR.position = 0
+
+    errors = 0xff
+    e_old = integral = 0
+    t_beg = t_old = time.time()
+    e = distance_count
+    k_p = 0.4
+    k_i = 0.7; max_i = 15
+    k_d = 0
+
+    while errors: 
+        t = time.time()
+         # increase to 90 over 1.5 seconds
+        max_power = max(min((t - t_beg)* 90/1.5, 90), -90)
+
+        dT = t - t_old
+        e = distance_count - int((mL.position + mR.position)/2)
+        errors = ((errors << 1) & 0xff) | (e != 0)
+        dE = e - e_old
+        u = k_p * e +  max(min(k_i * integral, max_i), -max_i) + k_d * dE / dT
+
+        u = max(min(u, max_power), -max_power)
+        debug_print(t, e, dE, integral, u , sep=', ')
+        mrsnod(u, u, mL, mR, gyro, angle)
+
+        integral += e * dT
+        integral = max(min(integral, max_i), -max_i)
+        e_old = e
+        t_old = t
+
+    mR.run_direct(duty_cycle_sp=0)
+    mL.run_direct(duty_cycle_sp=0)
+
+# motor_rotate_same_number_of_degrees
+def mrsnod(duty_cycle_l, duty_cycle_r, mL, mR, gyro, angle):
+    # Not the best way to do things ^^', in fact it probably wont work...
+    k_p = 0.5
+    e = (mL.position - mR.position) + (angle - get_angle(gyro, 2))
+    u = max(min(e * k_p, 10), -10)
+
+    mL.run_direct(duty_cycle_sp=duty_cycle_l - u)
+    mR.run_direct(duty_cycle_sp=duty_cycle_r + u)
+
+
 
 def main():
     '''The main function of our program'''
@@ -57,10 +146,24 @@ def main():
 
     debug_print("Gyro: " + str(gy.value()))
 
+
+    drive_for_centimeters(10, mL, mR, gy, 0)
+    rotate_to_angle(90, mL, mR, gy)
+    drive_for_centimeters(20, mL, mR, gy, 90)
+    rotate_to_angle(270, mL, mR, gy, )
+    drive_for_centimeters(20, mL, mR, gy, 270)
+    rotate_to_angle(45, mL, mR, gy)
+    rotate_to_angle(0, mL, mR, gy)
+    drive_for_centimeters(-10, mL, mR, gy, 0)
+
+    
+
+
+
     #Sound.speak('Hello, my name is EV3! Govorim tudi slovensko!').wait()
     #mL.run_to_rel_pos(position_sp= 840, speed_sp = 250)
     #mR.run_to_rel_pos(position_sp=-840, speed_sp = 250)
-    mL.wait_while('running')
+    """ mL.wait_while('running')
     mR.wait_while('running')
 
 
@@ -82,7 +185,7 @@ def main():
 
     time.sleep(3)
 
-    debug_print("Gyro: " + str(gy.value()))
+    debug_print("Gyro: " + str(gy.value())) """
 
     # print something to the screen of the device
     #print('Hello World!')
